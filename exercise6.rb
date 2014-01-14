b23dd057a15af656e7ef8d22496092488c06171c
janocee <janetcheung86@gmail.com>
2014-01-14 15:49:55 -0500

    step 2

--- exercise6.rb
+++ exercise6.rb
@@ -1,2 +1,13 @@
-grocery_list = ["*carrots", "*toilet paper", "*apples", "*salmon"]
-puts grocery_list
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+
+puts "What do you still need to buy?"
+newitem = gets.chomp
+grocery_list << newitem
+
+def add(list)
+	list.each do |x|
+		puts "*#{x}"
+	end
+end
+
+add(grocery_list)
