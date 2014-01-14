5fd6f0535c4ed650b12ff1696b6809c0b6bc9fd9
janocee <janetcheung86@gmail.com>
2014-01-14 16:25:36 -0500

    status 6

--- exercise6.rb
+++ exercise6.rb
@@ -1,9 +1,5 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
-puts "What do you still need to buy?"
-newitem = gets.chomp
-grocery_list << newitem
-
 def add(list)
 	list.each do |x|
 		puts "*#{x}"
@@ -18,8 +14,19 @@ def includes(food)
 	end
 end
 
+def sort(alpha)
+	alpha.sort!
+	add(alpha)
+end
+
 add(grocery_list)
-includes(grocery_list)
+grocery_list << "rice"
 
 puts "Total number of items are #{grocery_list.length}."
+
+includes(grocery_list)
+
 puts "The second item on the list is #{grocery_list[1]}."
+
+sort(grocery_list)
+
